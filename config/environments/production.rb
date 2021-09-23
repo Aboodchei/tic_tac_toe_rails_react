a8b384c5eb7fc7b4430a527653514cfc0293289c
Abdulla Mahmoud <aboodchei@gmail.com>
2021-09-23 19:53:25 +0200

    add configuration for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.web_socket_server_url = "wss://intense-temple-28283.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://intense-temple-28283.herokuapp.com', 'http://intense-temple-28283.herokuapp.com']
+
 end
